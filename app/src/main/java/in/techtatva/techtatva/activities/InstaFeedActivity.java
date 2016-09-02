35fc40bebe9d370c2c9eab5d867c05564d9bc732
Naman Yadav <namanyadav128@gmail.com>
2016-09-02 23:41:56 +0530

    Instagram Feed Added

--- app/src/main/java/in/techtatva/techtatva/activities/InstaFeedActivity.java
+++ app/src/main/java/in/techtatva/techtatva/activities/InstaFeedActivity.java
@@ -1,53 +1,69 @@
 package in.techtatva.techtatva.activities;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-
-
-import java.util.ArrayList;
-import java.util.List;
-
+import android.view.View;
+import chipset.potato.Potato;
 import in.techtatva.techtatva.R;
 import in.techtatva.techtatva.adapters.InstaFeedAdapter;
-import in.techtatva.techtatva.models.InstaFeedModel;
+import in.techtatva.techtatva.models.Instagram.InstagramFeed;
+import in.techtatva.techtatva.network.InstaFeedAPIClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class InstaFeedActivity extends AppCompatActivity {
+
+    InstagramFeed feed;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insta_feed);
+        context=this;
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.insta_tt16_hashtag);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-        RecyclerView instaFeedRecyclerView = (RecyclerView)findViewById(R.id.insta_feed_recycler_view);
-        InstaFeedAdapter adapter = new InstaFeedAdapter(this, getInstaFeedList());
-        instaFeedRecyclerView.setAdapter(adapter);
-        instaFeedRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        if (Potato.potate(this).Utils().isInternetConnected()){
+            loadFeed();
+        }
+        else{
+            View noConnectionLayout = findViewById(R.id.no_connection_layout);
+            noConnectionLayout.setVisibility(View.VISIBLE);
+        }
     }
 
-    private List<InstaFeedModel> getInstaFeedList() {
+    public void loadFeed()
+    {
+        final View progressDialog=findViewById(R.id.progress_dialog);
+        progressDialog.setVisibility(View.VISIBLE);
+        Call<InstagramFeed> call = InstaFeedAPIClient.getInterface().getInstagramFeed();
+        call.enqueue(new Callback<InstagramFeed>() {
+            @Override
+            public void onResponse(Call<InstagramFeed> call, Response<InstagramFeed> response) {
+                feed = response.body();
+                InstaFeedAdapter adapter = new InstaFeedAdapter(context, feed);
+                RecyclerView instaFeedRecyclerView=(RecyclerView)findViewById(R.id.insta_feed_recycler_view);
+                instaFeedRecyclerView.setAdapter(adapter);
+                instaFeedRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+                progressDialog.setVisibility(View.GONE);
+                instaFeedRecyclerView.setVisibility(View.VISIBLE);
+            }
 
-        InstaFeedModel model = new InstaFeedModel();
-        model.setDisplay(R.drawable.tt_logo);
-        model.setName("mittechtatva");
-        model.setImage(R.drawable.tt_insta_img);
-
-        model.setDescription("mittechtatva Fuel RC 4.0 #FuelRC #FeaturedEvent #TechTatva16");
-        model.setLikes(34);
-        model.setComments(0);
-
-        List<InstaFeedModel> list = new ArrayList<>();
-        list.add(model);
-        return list;
+            @Override
+            public void onFailure(Call<InstagramFeed> call, Throwable t) {
+                View noConnectionLayout = findViewById(R.id.no_connection_layout);
+                noConnectionLayout.setVisibility(View.VISIBLE);
+            }
+        });
     }
-
 
 }
---
+++ app/src/main/java/in/techtatva/techtatva/network/InstaFeedAPIClient.java
@@ -0,0 +1,31 @@
+package in.techtatva.techtatva.network;
+
+import in.techtatva.techtatva.models.Instagram.InstagramFeed;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+/**
+ * Created by Naman on 8/31/2016.
+ */
+public class InstaFeedAPIClient {
+
+    public static final String BASE_URL_INSTAGRAM="https://api.instagram.com/";
+    private static Retrofit retrofit = null;
+
+    public static APIInterface getInterface(){
+
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder().baseUrl(BASE_URL_INSTAGRAM).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+
+        return retrofit.create(APIInterface.class);
+    }
+
+    public interface APIInterface {
+        @GET("v1/tags/techtatva16/media/recent?access_token=630237785.f53975e.8dcfa635acf14fcbb99681c60519d04c")
+        Call<InstagramFeed> getInstagramFeed();
+
+    }
+}
