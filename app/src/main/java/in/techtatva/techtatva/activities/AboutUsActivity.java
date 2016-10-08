8de7e9b032c2fe0c7db54b6e9ad37f8160f4e3ae
Naman Yadav <namanyadav128@gmail.com>
2016-10-08 14:04:56 +0530

    Snapchat added

--- app/src/main/java/in/techtatva/techtatva/activities/AboutUsActivity.java
+++ app/src/main/java/in/techtatva/techtatva/activities/AboutUsActivity.java
@@ -1,9 +1,11 @@
 package in.techtatva.techtatva.activities;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -18,9 +20,6 @@ public class AboutUsActivity extends AppCompatActivity {
     final String FB_URL = "https://www.facebook.com/MITtechtatva";
     final String TWITTER_URL = "https://www.twitter.com/MITtechtatva";
     final String INSTA_URL = "https://www.instagram.com/MITtechtatva";
-    final String YOUTUBE_URL = "https://www.youtube.com/TechTatva";
-    final String GPLUS_URL = "https://plus.google.com/+TechTatva";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,22 +44,20 @@ public class AboutUsActivity extends AppCompatActivity {
                 Potato.potate(context).Intents().browserIntent(FB_URL);
             }
         });
-        findViewById(R.id.youtube_image).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Potato.potate(context).Intents().browserIntent(YOUTUBE_URL);
-            }
-        });
         findViewById(R.id.insta_image).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Potato.potate(context).Intents().browserIntent(INSTA_URL);
             }
         });
-        findViewById(R.id.gplus_image).setOnClickListener(new View.OnClickListener() {
+
+
+        findViewById(R.id.snapchat_image).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Potato.potate(context).Intents().browserIntent(GPLUS_URL);
+                new AlertDialog.Builder(context)
+                        .setMessage("@mittt16")
+                        .create().show();
             }
         });
 
