1f97615b00c26835bafc8c2232f9e76e0eb32944
Anuraag <baishya.anuraag1920@gmail.com>
2016-10-09 12:35:10 +0530

    Added Crashlytics, Answers, Build Tools Update, Library Updates, MultiDex Added

--- app/src/main/java/in/techtatva/techtatva/applications/TechTatva16.java
+++ app/src/main/java/in/techtatva/techtatva/applications/TechTatva16.java
@@ -1,7 +1,12 @@
 package in.techtatva.techtatva.applications;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
@@ -13,10 +18,16 @@ public class TechTatva16 extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(this).build();
         Realm.setDefaultConfiguration(realmConfiguration);
 
+    }
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 
 }
